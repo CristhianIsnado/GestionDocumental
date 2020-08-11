721f5d3ac63eafaf03434651dae1c05ecd2d6de4
santiago <santiago.23.ag@gmail.com>
2020-08-11 16:03:53 -0400

    titulo de introduccion cambiado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema Documental
                 </div>
 
                 <div class="links">
